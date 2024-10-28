a5e9488200630b7b98feaae5602bf42247669e05
agabin <agabin@appdev.yearup.org>
2024-10-28 11:15:29 -0400

    Updates

---
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.io.File;
+
+public class Dealership {
+    String name;
+    String address;
+    String phone;
+
+
+
+
+}
---
+++ CarDealership/src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.io.IOException;
+
+public class UserInterface {
+
+    Dealership dealership;
+
+    public UserInterface() throws IOException {
+
+    }
+
+
+}
