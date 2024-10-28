026ba0aa1c9da5adee7e56ddcbd7b27a9201b9b0
agabin <agabin@appdev.yearup.org>
2024-10-28 11:07:21 -0400

    Updates

---
+++ DealershipApplication/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
+
+    Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price){
+
+    }
+
+
+}
